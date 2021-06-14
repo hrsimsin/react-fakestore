d9d31eb676c175fb6fa9159e59b14ff2599890ed
harsimar17290 <harsimar17290@iiitd.ac.in>
2021-06-14 10:29:58 +0530

    Add add to cart button in product cards

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { RiLogoutBoxRFill } from "react-icons/ri";
 import ProductPage from './components/ProductPage/ProductPage';
 import CategoryPage from './components/CategoryPage/CategoryPage';
 import CartPage from './components/CartPage/CartPage';
-import CategoryProductPage from './components/CategoryPage/CategoryProductPage/CategoryProductPage';
+import CartContext from './context/CartContext';
 
 import {
   BrowserRouter as Router,
@@ -29,7 +29,7 @@ function App() {
     'bgc-bg-1': '#a8dadc',
     'txc-fr-1': '#f1faee',
     'txc-fr-2': '#f1faee',
-    'txc-bg-1': '#f1faee',
+    'txc-bg-1': '#000',
     toggle: () => setTheme(darkTheme)
   }
 
@@ -65,37 +65,67 @@ function App() {
         {
           name: 'Logout',
           icon: <RiLogoutBoxRFill />,
-          link:'/logout'
+          link: '/logout'
         }
-      ]      
+      ]
     });
 
   const [theme, setTheme] = useState(lightTheme);
 
+  const [cart, setCart] = useState({
+    products: [],
+    addProduct: (product) => cart.products.push({ ...product, quantity: 1 }),
+    increaseProductQuantity: (product) => {
+      const prd = cart.products.filter(el => el.id === product.id)[0];
+      if (prd)
+        prd.quantity += 1;
+    },
+    decreaseProductQuantity: (product) => {
+      const prd = cart.products.filter(el => el.id === product.id)[0];
+      if (prd) {
+        if (prd.quantity === 1) {
+          cart.products = cart.products.filter(el => el.id !== product.id);
+        }
+        else {
+          prd.quantity -= 1;
+        }
+      }
+    },
+    getProductQuantity: (product) => {
+      const prd = cart.products.filter(el => el.id === product.id)[0];
+      if (prd) {
+        return prd.quantity;
+      }
+      return (0);
+    }
+  });
+
   return (
     <MenuContext.Provider value={menu}>
       <ThemeContext.Provider value={theme}>
-        <Router onC>
-          <div className="main-container">
-            <Header />
-            <main style={{ backgroundColor: theme['bgc-bg-1'] }}>
-              <Switch>
-                <Route exact path="/">
-                  <Redirect to="/products" />
-                </Route>
-                <Route path="/products">
-                  <ProductPage />
-                </Route>
-                <Route path="/categories">
-                  <CategoryPage />
-                </Route>
-                <Route path="/cart">
-                  <CartPage />
-                </Route>
-              </Switch>
-            </main>
-          </div>
-        </Router>
+        <CartContext.Provider value={cart}>
+          <Router>
+            <div className="main-container">
+              <Header />
+              <main style={{ backgroundColor: theme['bgc-bg-1'] }}>
+                <Switch>
+                  <Route exact path="/">
+                    <Redirect to="/products" />
+                  </Route>
+                  <Route path="/products">
+                    <ProductPage />
+                  </Route>
+                  <Route path="/categories">
+                    <CategoryPage />
+                  </Route>
+                  <Route path="/cart">
+                    <CartPage />
+                  </Route>
+                </Switch>
+              </main>
+            </div>
+          </Router>
+        </CartContext.Provider>
       </ThemeContext.Provider>
     </MenuContext.Provider>
   );
--- src/components/ProductGrid/ProductCard/ProductCard.js
+++ src/components/ProductGrid/ProductCard/ProductCard.js
@@ -1,23 +1,74 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import ThemeContext from '../../../context/ThemeContext';
 import './ProductCard.css';
+import { FaCartPlus } from 'react-icons/fa';
+import CartContext from '../../../context/CartContext';
 
 function ProductCard(props) {
     const theme = useContext(ThemeContext);
+    const cart = useContext(CartContext);
+
+    const [quantity, setQuantity] = useState(0);
+
+    useEffect(() => {
+        setQuantity(cart.getProductQuantity(props.product));
+    }, []);
 
     return (
         <div className="product-card" style={{ backgroundColor: theme['bgc-fr-2'] }}>
+
             <h3 style={{ color: theme['txc-fr-2'] }}>{props.product.title}</h3>
             <div className="image">
                 <img src={props.product.image} />
             </div>
-            
+
             <p style={{ color: theme['txc-fr-2'] }}>
                 {
-                props.product.description.length > 290 ? `${props.product.description.substr(0,289)}...` :  props.product.description 
+                    props.product.description.length > 290 ? `${props.product.description.substr(0, 289)}...` : props.product.description
                 }
             </p>
-            <p style={{ color: theme['txc-fr-2'] }} className="price">${props.product.price}</p>
+            <div className="bottom-row">
+                <p style={{ color: theme['txc-fr-2'] }} className="price">${props.product.price}</p>
+                {
+                    (quantity == 0) &&
+                    <button onClick={
+                        () => { cart.addProduct(props.product); setQuantity(quantity + 1); }
+                    } style={{
+                        backgroundColor: theme['bgc-fr-1'],
+                        color: theme['txc-fr-1']
+                    }}>
+                        <FaCartPlus />
+                    </button>
+                }
+                {
+                    (quantity != 0) &&
+                    <div className="quantity-control">
+                        <button className="btn-increase" onClick={
+                            () => { cart.increaseProductQuantity(props.product); setQuantity(quantity+1); }
+                        } style={{
+                            backgroundColor: theme['bgc-fr-1'],
+                            color: theme['txc-fr-1']
+                        }}>
+                            +
+                        </button>
+                        <div className="quantity" style={{
+                            backgroundColor:theme['bgc-bg-1'],
+                            color: theme['txc-bg-1']
+                        }}>
+                            {quantity}
+                        </div>
+                        <button className="btn-decrease" onClick={
+                            () => { cart.decreaseProductQuantity(props.product); setQuantity(quantity-1); }
+                        }
+                         style={{
+                            backgroundColor: theme['bgc-fr-1'],
+                            color: theme['txc-fr-1']
+                        }}>
+                            -
+                        </button>
+                    </div>
+                }
+            </div>
         </div>
     );
 }
