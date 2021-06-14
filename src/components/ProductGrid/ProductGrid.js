a1e40041b437a21de10eb6dc0b61cadbc805bd7d
harsimar17290 <harsimar17290@iiitd.ac.in>
2021-06-14 18:08:39 +0530

    Add product search

--- src/components/ProductGrid/ProductGrid.js
+++ src/components/ProductGrid/ProductGrid.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './ProductGrid.css';
 import ProductCard from './ProductCard/ProductCard';
+import ThemeContext from '../../context/ThemeContext';
 
 function ProductGrid(props) {
-    return (
-        <div className="grid"> 
-        {   
-            props.products.map((el,index,arr) => <ProductCard product={el} key={index} />)
+    const theme = useContext(ThemeContext);
+
+    const [input, setInput] = useState("");
+    const [displayProducts,setDisplayProducts] = useState(props.products);
+
+    useEffect(()=>{
+        if(!input){
+            setDisplayProducts(displayProducts);
         }
-    </div>
+        else{
+            setDisplayProducts(props.products.filter(el => {
+               return el.title.toLowerCase().includes(input.toLowerCase());
+            }));
+        }
+    },[input]);
+
+    return (
+        <div className="product-grid-container">
+
+            <input  
+            placeholder="Search for Products"
+            name="search-input"
+            value={input} 
+            onChange={(e) => setInput(e.target.value)}
+            style={{
+                backgroundColor:theme['bgc-fr-2'],
+                color:theme['txc-fr-2'],
+                border:`3px solid ${theme['acc']}`
+            }}
+            className="product-search" />
+
+            <div className="grid">
+                {
+                    displayProducts.map((el, index, arr) => <ProductCard product={el} key={index} />)
+                }
+            </div>
+        </div>
     );
 }
 
